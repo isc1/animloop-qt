e50eb19f392fc1f2e4c1f36e775743d8a4a708f1
isc1 <isc1@inhabited.systems>
2017-09-25 12:41:07 -0600

    added comments to main.cpp

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+    MainWindow w;  // most of the program lives in mainwindow.cpp and mainwindow.h
     w.show();
 
-    return a.exec();
+    return a.exec();  // hand over control of our app to Qt engine
 }
